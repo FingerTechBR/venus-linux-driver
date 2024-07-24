fb045c547f86b62eec5d68014c2da78aff9cd141
Lucas A. <drea.pierra@gmail.com>
2024-07-24 17:32:34 -0300

    added the nbiobsp library to the installer

--- install-driver.sh
+++ install-driver.sh
@@ -27,9 +27,9 @@ fi
 drvkernel=$(/sbin/modinfo VenusDrv.ko | grep vermagic | cut -f8 -d " ")
 if [ $drvkernel != $KER_VER ]; then
   echo "Your kernel version $KER_VER does not match with NITGEN USB Driver compiled kernel version $drvkernel"
-  read -p "Do you want to proceed anyways? (y/n) " choice
+  read -p "Do you want to proceed anyways? (Y/n) " choice
   case "$choice" in
-    y|Y ) ;;
+    y|Y|"") ;;
     n|N ) exit 1;;
     * ) echo "Invalid choice, exiting..."; exit 1;;
   esac
@@ -57,15 +57,33 @@ fi
 arch=$(uname -m)
 if [ "$arch" = "i686" ] || [ "$arch" = "i386" ]; then
   if [ -f ./shared-object/x86/VenusLib.so ]; then
-    cp ./shared-object/x86/VenusLib.so /lib/
+    [ -f /lib/VenusLib.so ] && echo "Overwriting VenusLib.so for x86 architecture."
+    cp -f ./shared-object/x86/VenusLib.so /lib/
+    for lib in ./sdk/x86/eNBSP/bin/lib*.so; do
+      [ -f "/lib/$(basename "$lib")" ] && echo "Overwriting $(basename "$lib") for x86 architecture."
+      cp -f "$lib" /lib/
+    done
+    find ./sdk/x86/eNBSP/bin/ -type f ! -name 'lib*' | while read file; do
+      [ -f /usr/local/bin/$(basename "$file") ] && echo "Overwriting $(basename "$file") for x86 architecture."
+      cp -f "$file" /usr/local/bin/
+    done
   else
-    echo "File VenusLib.so does not exist in the shared-object/x86 directory"
+    echo "File VenusLib.so does not exist in the current directory and was not copied"
   fi
 else
   if [ -f ./shared-object/VenusLib.so ]; then
-    cp ./shared-object/VenusLib.so /lib/
+    [ -f /lib/VenusLib.so ] && echo "Overwriting VenusLib.so for x64 architecture."
+    cp -f ./shared-object/VenusLib.so /lib/
+    for lib in ./sdk/x64/eNBSP/bin/lib*.so; do
+      [ -f "/lib/$(basename "$lib")" ] && echo "Overwriting $(basename "$lib") for x64 architecture."
+      cp -f "$lib" /lib/
+    done
+    find ./sdk/x64/eNBSP/bin/ -type f ! -name 'lib*' | while read file; do
+      [ -f /usr/local/bin/$(basename "$file") ] && echo "Overwriting $(basename "$file") for x64 architecture."
+      cp -f "$file" /usr/local/bin/
+    done
   else
-    echo "File VenusLib.so does not exist in the current directory"
+    echo "File VenusLib.so does not exist in the current directory and was not copied"
   fi
 fi
 
